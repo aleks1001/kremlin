52a47248ddce41b7a88d089d8474a965c0c47f9d
Kozyachiy, Alexey <alexey.kozyachiy@priceline.com>
2018-02-26 16:48:17 -0500

    some routes

--- main.go
+++ main.go
@@ -12,6 +12,7 @@ import (
 
 	h "github.com/aleks1001/kremlin/src"
 	"encoding/json"
+	"strconv"
 )
 
 var (
@@ -38,6 +39,18 @@ var routes = Routes{
 		"/hotel",
 		postHotel,
 	},
+	Route{
+		"Hotels",
+		"POST",
+		"/hotels",
+		postHotels,
+	},
+	Route{
+		"GetByCityId",
+		"GET",
+		"/city/{id}",
+		getByCityId,
+	},
 }
 
 type Route struct {
@@ -49,19 +62,25 @@ type Route struct {
 
 type Routes []Route
 
-func main() {
+func init() {
 	var err error
-	session, err = r.Connect(r.ConnectOpts{Address: "localhost:28015"})
+	session, err = r.Connect(r.ConnectOpts{
+		Address:  "localhost:28015",
+		Database: "test",
+		MaxOpen:  40,
+	})
 	if err != nil {
-		log.Fatal("Database connection failed:", err)
+		log.Fatalln(err.Error())
 	}
+}
 
+func main() {
 	ipPtr := flag.String("ip", "0.0.0.0", "localhost")
 	portPtr := flag.String("port", "3232", "port to listen on")
 	server := newServer(fmt.Sprintf("%v:%v", *ipPtr, *portPtr))
 	msg := fmt.Sprintf("Starting server on %s:%s", *ipPtr, *portPtr)
 	log.Println(msg)
-	err = server.ListenAndServe()
+	err := server.ListenAndServe()
 	if err != nil {
 		log.Fatalln("Error: %v", err)
 	}
@@ -89,8 +108,67 @@ func newServer(addr string) *http.Server {
 	}
 }
 
+func getByCityId(w http.ResponseWriter, req *http.Request) {
+	vars := mux.Vars(req)
+	id, err := strconv.ParseFloat(vars["id"], 64)
+
+	var hotels h.Hotels
+	res, err := r.Table("hotel").Filter(r.Row.Field("CityId").Eq(id)).OrderBy(r.Desc("OverallGuestRating")).Run(session)
+	if err != nil {
+		http.Error(w, err.Error(), http.StatusInternalServerError)
+		return
+	}
+	defer res.Close()
+	err = res.All(&hotels)
+	if err != nil {
+		http.Error(w, err.Error(), http.StatusInternalServerError)
+		return
+	}
+
+	js, err := json.Marshal(hotels)
+	if err != nil {
+		http.Error(w, err.Error(), http.StatusInternalServerError)
+		return
+	}
+
+	w.Header().Set("Content-Type", "application/json")
+	w.Write(js)
+}
+
 func getAppStatus(w http.ResponseWriter, req *http.Request) {
 	w.WriteHeader(http.StatusOK)
+}
+
+func postHotels(w http.ResponseWriter, req *http.Request) {
+	body, err := ioutil.ReadAll(req.Body)
+	defer req.Body.Close()
+	if err != nil {
+		http.Error(w, err.Error(), http.StatusInternalServerError)
+		log.Fatal(err)
+		return
+	}
+	var hotels h.HotelsResponse
+	json.Unmarshal(body, &hotels)
+	for _, hotel := range hotels.Hotels {
+		var h = &h.Hotel{
+			CheckIn:hotels.CheckIn,
+			CheckOut:hotels.CheckOut,
+			HotelId:hotel.HotelId,
+			BrandId:hotel.BrandId,
+			HotelName:hotel.HotelName,
+			NeighborhoodId:hotel.Location["neighborhoodId"].(string),
+			NeighborhoodName:hotel.Location["neighborhoodName"].(string),
+			CityId:hotel.Location["cityId"].(float64),
+			OverallGuestRating:hotel.OverallGuestRating,
+			MinPrice:hotel.RatesSummary["minPrice"].(string),
+			StarRating:hotel.StarRating,
+			TotalReviewCount:hotel.TotalReviewCount,
+		}
+		_, err = r.DB("test").Table("hotel").Insert(h).RunWrite(session)
+	}
+
+	w.Header().Set("Content-Type", "application/json")
+	w.Write(body)
 }
 
 func postHotel(w http.ResponseWriter, req *http.Request) {
@@ -101,14 +179,10 @@ func postHotel(w http.ResponseWriter, req *http.Request) {
 		log.Fatal(err)
 		return
 	}
-	var hotels h.Hotels
-	json.Unmarshal(body, &hotels)
+	var hotel h.HotelResponse
+	json.Unmarshal(body, &hotel)
 
-	fmt.Println(len(hotels.Hotels))
-
-	for _, hotel := range hotels.Hotels {
-		_, err = r.DB("test").Table("hotel").Insert(hotel).RunWrite(session)
-	}
+	_, err = r.Table("hotel").Insert(hotel).RunWrite(session)
 
 	w.Header().Set("Content-Type", "application/json")
 	w.Write(body)
--- src/hotel.go
+++ src/hotel.go
@@ -1,13 +1,40 @@
 package hotel
 
-type Hotels struct {
-	Hotels []Hotel `json:"hotels"`
+import "time"
+
+type HotelsResponse struct {
+	Hotels   []HotelResponse `json:"hotels"`
+	CheckIn  time.Time `json:"checkIn"`
+	CheckOut time.Time `json:"checkOut"`
 }
 
+type HotelResponse struct {
+	HotelId            string        `json:"hotelId"`
+	HotelName          string        `json:"name"`
+	Brand              string        `json:"brand"`
+	BrandId            string        `json:"brandId"`
+	StarRating         int           `json:"starRating"`
+	TotalReviewCount   int        `json:"totalReviewCount"`
+	OverallGuestRating float64         `json:"overallGuestRating"`
+	RatesSummary       map[string]interface{} `json:"ratesSummary"`
+	HotelFeatures      map[string]interface{} `json:"hotelFeatures"`
+	Location           map[string]interface{}        `json:"location"`
+	HtlDealScore       int32        `json:"htlDealScore"`
+}
+
+type Hotels []Hotel
+
 type Hotel struct {
-	HotelId       string `json:"hotelId"`
-	HotelName     string `json:"name"`
-	StarRating    int `json:"starRating"`
-	RatesSummary  map[string]interface{} `json:"ratesSummary"`
-	HotelFeatures map[string]interface{} `json:"hotelFeatures"`
+	CheckIn            time.Time
+	CheckOut           time.Time
+	HotelId            string
+	BrandId            string
+	HotelName          string
+	NeighborhoodId     string
+	NeighborhoodName   string
+	CityId             float64
+	OverallGuestRating float64
+	MinPrice           string
+	StarRating         int
+	TotalReviewCount   int
 }
